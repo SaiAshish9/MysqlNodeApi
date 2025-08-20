26e001915e692954f4ead6e9a6a93f4a665d5a0a
SaiAshish <saiashish3760@gmail.com>
2025-08-20 12:36:20 +0530

    k

--- routes/routes.js
+++ routes/routes.js
@@ -1,7 +1,6 @@
 const express = require('express')
-const db = require('../db/index')
-
 const router = express.Router()
+const db = require('../db/index')
 
 router.get('/', async (req, res, next) => {
   try {
