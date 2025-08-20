1647094dbf851872b63b6ba3d92e70009b37882c
SaiAshish <saiashish3760@gmail.com>
2025-08-20 08:58:13 +0530

    k

--- server.js
+++ server.js
@@ -3,11 +3,7 @@ const app=express()
 const routes=require('./routes/routes')
 
 app.use(express.json())
-
 app.use('/api/',routes)
-
-
-
 
 app.listen(process.env.PORT||3000,()=>{
   console.log("server started");
